a2d80412805f106f60351fee4d3c0fee50272a13
Cliff Rice <crice@iunu.com>
2025-07-02 16:10:44 -0400

    oops

--- mcp-api/dist/database/entities/book.entity.d.ts
+++
@@ -1,8 +0,0 @@
-import { Author } from './author.entity';
-export declare class Book {
-    id: string;
-    title: string;
-    description: string;
-    price: number;
-    author: Author;
-}
--- mcp-api/dist/database/entities/order.entity.d.ts
+++
@@ -1,7 +0,0 @@
-import { OrderItem } from './order-item.entity';
-export declare class Order {
-    id: string;
-    userId: string;
-    createdAt: Date;
-    items: OrderItem[];
-}
